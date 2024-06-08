fd4c53e1b094ca4662886edf7b76eac6f11345b6
joshuadesierto <desiertojl98@gmail.com>
2024-06-08 14:31:45 +0800

    New Feature : Update and Delete - June 8, 2024

--- src/main/java/com/eeezi/ezziblogrestapi/exception/BlogAPIException.java
+++ src/main/java/com/eeezi/ezziblogrestapi/exception/BlogAPIException.java
@@ -9,6 +9,10 @@ public class BlogAPIException extends RuntimeException{
     private HttpStatus httpStatus;
     private String message;
 
+    public BlogAPIException(HttpStatus httpStatus, String message){
+        this.httpStatus = httpStatus;
+        this.message = message;
+    }
 
     public BlogAPIException(HttpStatus httpStatus, String message, String message1){
         super(message);
