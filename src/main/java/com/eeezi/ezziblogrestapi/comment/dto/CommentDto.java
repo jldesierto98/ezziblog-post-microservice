126b0dca68ce8cb8013ffc6860ec36cfbfab8a97
joshuadesierto <desiertojl98@gmail.com>
2024-07-13 22:46:24 +0800

    Swagger Documentation Improvement

--- src/main/java/com/eeezi/ezziblogrestapi/comment/dto/CommentDto.java
+++ src/main/java/com/eeezi/ezziblogrestapi/comment/dto/CommentDto.java
@@ -1,6 +1,7 @@
 package com.eeezi.ezziblogrestapi.comment.dto;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
